c9d7d96dea89c980b2adf9c30c48f6075f16eff1
Kazuhiro Kojio <kazuhiro.kojio@gmail.com>
2020-02-17 19:30:09 -0300

    remove books routes

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import config from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import bookRoutes from './server/routes/BookRoutes';
 import userRoutes from './server/routes/UserRoutes';
 import offerRoutes from './server/routes/OfferRoutes';
 
@@ -14,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 8000;
 
-app.use('/api/v1/books', bookRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/offers', offerRoutes);
 
